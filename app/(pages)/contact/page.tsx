d2ae507b2cacea755b83ed2c4413391d0a739d7a
Iqyan Aziz Syamaidzar <azizsyam503@gmail.com>
2024-09-23 14:58:10 +0700

    create contact page

--- app/(pages)/contact/page.tsx
+++ app/(pages)/contact/page.tsx
@@ -1,7 +1,14 @@
+import GetInTouch from "@/components/GetInTouch";
 import React from "react";
 
 const page = () => {
-  return <div>Contact</div>;
+  return (
+    <main className="bg-gray-100">
+      <div className="max-padd-container">
+        <GetInTouch />
+      </div>
+    </main>
+  );
 };
 
 export default page;
